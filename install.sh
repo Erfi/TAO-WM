32b721ea4b5181c79c25e63c602ab0682bd439e6
Erfan Azad <erfan.basiri.azad@gmail.com>
2025-07-29 17:07:51 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,19 @@
+#!/bin/bash
+cd external
+
+# Install lumos submodule and its dependencies
 cd lumos
 git submodule update --init --recursive
 sh install.sh
+
 cd ..
+
+# Install calvin_env submodule and its dependencies
+cd calvin_env
+git submodule update --init --recursive
+pip install --no-cache-dir -e .
+
+cd ../..
+
+# Install the main package
 pip install --no-cache-dir -e .
